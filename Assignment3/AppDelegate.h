2720ad2a8dbc4fe1b5c744a0249e3c6a6799913a
Build User <builduser@ci-osx-executor.local>
2014-02-04 11:02:05 -0500

    Initial Commit

---
+++ Assignment3/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Assignment3
+//
+//  Created by Build User on 2/4/14.
+//  Copyright (c) 2014 Pitt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Assignment3/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Assignment3
+//
+//  Created by Build User on 2/4/14.
+//  Copyright (c) 2014 Pitt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
