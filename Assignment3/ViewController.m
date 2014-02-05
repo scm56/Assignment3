cd0b8099b494ca2ae8bf77d191c402d375e42008
Jay Palat <palatv@upmc.edu>
2014-02-04 21:02:08 -0500

    Add TableView Delegate and DataSource methods

--- Assignment3/ViewController.m
+++ Assignment3/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "Fruit.h"
 
 @interface ViewController ()
 
@@ -24,6 +25,41 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(int) numberOfSectionsInTableView:(UITableView *)tableView{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [_cart count];
+}
+-(NSString *) tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    return @"Fruit";
+}
+
+-(UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:@"TableViewCell"];
+    if (cell == Nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:@"TableViewCell"];
+    }
+    Fruit *tempFruit = [_cart objectAtIndex:indexPath.row];
+    
+    cell.textLabel.text = [tempFruit name];
+    cell.detailTextLabel.text = [tempFruit color];
+    
+    if (_allSelected) {
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    } else{
+        cell.accessoryType = UITableViewCellAccessoryNone;
+    }
+    
+    
+    
+    return cell;
 }
 
 @end
